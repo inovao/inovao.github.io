eb53d4f0647ed366abc6871b14c082e13e821ac7
inovao <31061655+inovao@users.noreply.github.com>
2017-11-29 20:37:56 +0200

    er

--- kiktv/js/custom.js
+++ kiktv/js/custom.js
@@ -7,10 +7,84 @@ $(window).load(function(){
 
 $(function(){
 
+	//fixed header and footer
+	var hf = function(){
+		var h_header = $('header').height();
+		var h_footer = $('footer').height();
+		$('.content').css({
+			'paddingTop': h_header,
+			'paddingBottom': h_footer
+		});
+
+	}
+
+	$(window).bind('load resize', hf);
+
+
+	//Click event to scroll to top
+	$('.up').click(function() {
+	    $('html, body').animate({
+	        scrollTop: 0
+	    }, 800);
+	    return false;
+	});
+
+
+	//form styler
     if($('.styler').length){
         $('.styler').styler();
     };
 
+    //click like
+    $('.like').click(function(){
+    	var like = $(this).find('span');
+    	if(like.hasClass('icon-like') == true ) {
+    		$(this).find('.icon-like').removeClass('icon-like').addClass('icon-like-actiive')
+    	} else if (like.hasClass('icon-like-actiive') == true ) {
+			$(this).find('.icon-like-actiive').removeClass('icon-like-actiive').addClass('icon-like')
+    	}
+    	
+    });
+
+    $('.js-sliding').click(function(){
+    	$(this).parents('.channel-list').toggleClass('show');
+    	return false;
+    })
+
+     $('.toggle-filter-search').click(function(){
+    	$(this).parents('.filter-search').find('.filter-search-wrap').toggleClass('show');
+    	$(this).toggleClass('active');
+    	return false;
+    })
+
+    //scrollbar
+	if($('.scrollbar-rail').length){
+
+		jQuery('.scrollbar-rail').scrollbar();
+	};
+    
+    
+    //Slider top
+    $('.kik-top-slider').slick({
+		infinite: false,
+		speed: 300,
+		slidesToShow: 4,
+		prevArrow: '<button class="slick-arrow slick-prev"><span class="icon-left-arrow"></span></button>',
+		nextArrow: '<button class="slick-arrow slick-next"><span class="icon-right-arrow"></span></button>',
+		variableWidth: true,
+		responsive: [
+		    {
+				breakpoint: 1680,
+				settings: {
+					slidesToShow: 3,
+
+				}
+			}
+			
+		]
+	});
+
+	//Trend slider
     $('.trend-slider').slick({
 		slidesToShow: 4,
 		slidesToScroll: 1,
@@ -29,7 +103,17 @@ $(function(){
 		]
     });
 
+    //Channel sldier
+    $('.channel-slider').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		infinite: false,
+		dots: true,
+		prevArrow: '<button class="slick-arrow slick-prev"><span class="icon-left-arrow"></span></button>',
+		nextArrow: '<button class="slick-arrow slick-next"><span class="icon-right-arrow"></span></button>',
+    });
 
+    //SearchBar
     $("#search-input-js").on("change keyup", function() {
 		$('.search-bar').addClass('open');
 		if($(this).val() == '') {
@@ -45,17 +129,18 @@ $(function(){
       event.stopPropagation();
     });
 
-    
+   
+    //  Tabs
+   	$('.replays-tabs a').click(function(){
+		$(this).parents('.replays-tab-wrap').find('.replays-tab-cont').addClass('hide');
+		$(this).parent().siblings().removeClass('active');
+		var id = $(this).attr('href');
+		$(id).removeClass('hide');
+		$(this).parent().addClass('active');
+		return false
+	});
 
-    $('.channel-slider').slick({
-		slidesToShow: 1,
-		slidesToScroll: 1,
-		infinite: false,
-		dots: true,
-		prevArrow: '<button class="slick-arrow slick-prev"><span class="icon-left-arrow"></span></button>',
-		nextArrow: '<button class="slick-arrow slick-next"><span class="icon-right-arrow"></span></button>',
-    });
-
+	//  Tabs
    	$('.tabs a').click(function(){
 		$(this).parents('.tab-wrap').find('.tab-cont').addClass('hide');
 		$(this).parent().siblings().removeClass('active');
@@ -67,7 +152,23 @@ $(function(){
 		return false
 	});
 
+   	if($('[data-mh]').length){
+	   	$(function() {
+	        resetTestOptions();
+	    });
 
+
+
+	    var resetTestOptions = function() {
+	        // update byRow option
+	        var byRow = $('body').hasClass('test-rows');
+	        $.each($.fn.matchHeight._groups, function() {
+	            this.options.byRow = byRow;
+	        });
+
+
+	    };
+    };
 });
 
    
