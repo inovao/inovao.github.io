080a4f7209d7a08e3d4cece1de6e8f2e74da9999
inovao <31061655+inovao@users.noreply.github.com>
2018-04-04 14:42:10 +0300

    rock

--- rock/js/custom.js
+++ rock/js/custom.js
@@ -36,6 +36,68 @@ $(function(){
 	    });
 	};
 
+
+	// carousels
+
+    var offer = $('.js-offer');
+    if (offer.length) {
+        var autoPlay = function autoPlay() {
+            interval = setInterval(function () {
+                var next = $('.js-offer-dot.active').next();
+                if (next.length) {
+                    next.trigger('click');
+                } else {
+                    dot.first().trigger('click');
+                }
+            }, 5000);
+        };
+
+        // count height
+        var countMaxHeight = function countMaxHeight() {
+            var maxHeight = Math.max.apply(null, item.map(function () {
+                return $(this).height();
+            }).get());
+
+            slider.height(maxHeight);
+        };
+
+        var slider = offer.find('.js-offer-slider'),
+            item = offer.find('.js-offer-item'),
+            dot = offer.find('.js-offer-dot'),
+            activeIndex = 0;
+
+        // click on dot
+        dot.on('click', function (e) {
+            var _this = $(this),
+                index = _this.index();
+
+            item.removeClass('active active-before');
+            item.eq(activeIndex).addClass('active-before');
+            item.eq(index).addClass('active');
+
+            dot.removeClass('active');
+            _this.addClass('active');
+
+            activeIndex = index;
+
+            // autoplay
+            if (interval) {
+                clearInterval(interval);
+            }
+            autoPlay();
+        });
+
+        var interval;
+
+        autoPlay();
+        countMaxHeight();
+
+        $(window).resize(function () {
+            countMaxHeight();
+        });
+    }
+
+
 	
 
 });
