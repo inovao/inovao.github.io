d6616e2c6586c11c6b9495af50bcc3c7a40e98c8
inovao <31061655+inovao@users.noreply.github.com>
2018-04-17 17:22:18 +0300

    upkick

--- upkick/js/custom.js
+++ upkick/js/custom.js
@@ -7,6 +7,13 @@ $(window).on('load', function(){
 
 $(function(){
 
+	/* ---------------------------------------------- /*
+	 * Styler
+	/* ---------------------------------------------- */
+    if($('.styler').length){
+        $('.styler').styler();
+    };
+
 
     /* ---------------------------------------------- /*
 	 * Carousel
@@ -104,6 +111,70 @@ $(function(){
 	}
 	});
 
+	/* ---------------------------------------------- /*
+    * MultiSelect
+    /* ---------------------------------------------- */
+	$(".mutli-select-head").on('click', function() {
+		$(this).parent().find(".mutli-select-list").slideToggle('fast');
+	});
+
+	function getSelectedValue(id) {
+		return $("#" + id).find("dt a span.value").html();
+	}
+
+	$(document).bind('click', function(e) {
+		var $clicked = $(e.target);
+		if (!$clicked.parents().hasClass("mutli-select")) $(".mutli-select-list").hide();
+	});
+
+	$('.mutli-select-list label').on('click', function() {
+
+		var title = $(this).find('input[type="checkbox"]').closest('.mutli-select-list').find('input[type="checkbox"]').val(),
+			title = $(this).find('input[type="checkbox"]').val() + ",";
+
+		if ($(this).find('.jq-checkbox').hasClass('checked')) {
+			var html = '<span title="' + title + '">' + title + '</span>';
+			$('.mutli-select-head__text').append(html);
+			
+			$(this).parents('.mutli-select').addClass('open');
+		} else {
+			$('span[title="' + title + '"]').remove();
+			
+
+		}
+		if ($('.mutli-select-head__text').text() == "") {
+			$(this).parents('.mutli-select').removeClass('open');
+		} 
+	});
+	/* ---------------------------------------------- /*
+    * Focus input
+    /* ---------------------------------------------- */
+
+
+    $('.form-input').focus(function(){
+        var label = $(this).next('label');
+        var width = $(label).width();
+        var adjust = width;
+        var value = $(this).val();
+ 
+        if(value == ''){
+            label.stop().animate({ 'margin-left': '-'+adjust, 'left': '100%', }, '300');
+            $(this).css({'padding-right': width + 20})
+            $(this).parent().addClass('focus')
+        } else {
+            label.css({ 'left':'-'+adjust });
+            
+        }
+    }).blur(function(){
+        var label = $(this).next('label');
+        var value = $(this).val();
+        if(value == ''){
+            label.stop().animate({ 'margin-left':'0', 'left': '0', }, '300');
+            $(this).removeAttr('style')
+            $(this).parent().removeClass('focus')
+
+        } 
+    });
 
 	/* ---------------------------------------------- /*
 	 * Toggle menu
