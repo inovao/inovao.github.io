1ec88b2215b353eeaa6c97448f2d5b56962c70c5
inovao <31061655+inovao@users.noreply.github.com>
2018-09-04 23:02:43 +0300

    fix

--- huboo/js/custom.js
+++ huboo/js/custom.js
@@ -40,34 +40,36 @@ $(function(){
     }
     $('input').autoClear();
 
-	$('.comments-slider').slick({
-		slidesToShow: 2, 
-		slidesToScroll: 1,
-		infinite: false,
-		autoplay: true,
-		autoplaySpeed: 2000,
-		prevArrow: '<button class="slick-arrow slick-prev"><svg class="icon icon-slick-left"><use xlink:href="#icon-slick-left"></use></svg></button>',
-		nextArrow: '<button class="slick-arrow slick-next"><svg class="icon icon-slick-right"><use xlink:href="#icon-slick-right"></use></svg></button>',
-		responsive: [
-			{
-			  breakpoint: 980,
-			  settings: {
-				arrows: false,
-			    dots: true
-			  }
-			},
-			{
-			  breakpoint: 736,
-			  settings: {
-			    slidesToShow: 1,
-			    slidesToScroll: 1,
-			    arrows: false,
-			    dots: true
-			  }
-			},
-			
-		]
-	})
+    if($('.comments-slider').length){
+		$('.comments-slider').slick({
+			slidesToShow: 2, 
+			slidesToScroll: 1,
+			infinite: false,
+			autoplay: true,
+			autoplaySpeed: 2000,
+			prevArrow: '<button class="slick-arrow slick-prev"><svg class="icon icon-slick-left"><use xlink:href="#icon-slick-left"></use></svg></button>',
+			nextArrow: '<button class="slick-arrow slick-next"><svg class="icon icon-slick-right"><use xlink:href="#icon-slick-right"></use></svg></button>',
+			responsive: [
+				{
+				  breakpoint: 980,
+				  settings: {
+					arrows: false,
+				    dots: true
+				  }
+				},
+				{
+				  breakpoint: 736,
+				  settings: {
+				    slidesToShow: 1,
+				    slidesToScroll: 1,
+				    arrows: false,
+				    dots: true
+				  }
+				},
+				
+			]
+		})
+	}
 
 	$('a.anchor').bind('click.smoothscroll',function () {
 		var target = $(this).attr('href'),
@@ -141,48 +143,79 @@ $(function(){
 		return false;
 	});
 
-	 (function(){
-	var a = document.querySelector('.pricing-nav-menu'), b = null, P = 150;
-	window.addEventListener('scroll', Ascroll, false);
-	document.body.addEventListener('scroll', Ascroll, false);
-	function Ascroll() {
-	  if (b == null) {
-	    var Sa = getComputedStyle(a, ''), s = '';
-	    for (var i = 0; i < Sa.length; i++) {
-	      if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
-	        s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
-	      }
-	    }
-	    b = document.createElement('div');
-	    b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
-	    a.insertBefore(b, a.firstChild);
-	    var l = a.childNodes.length;
-	    for (var i = 1; i < l; i++) {
-	      b.appendChild(a.childNodes[1]);
-	    }
-	    a.style.height = b.getBoundingClientRect().height + 'px';
-	    a.style.padding = '0';
-	    a.style.border = '0';
-	  }
-	  var Ra = a.getBoundingClientRect(),
-	      R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('.pricing-content').getBoundingClientRect().bottom);  // селектор блока, при достижении нижнего края которого нужно открепить прилипающий элемент
-	  if ((Ra.top - P) <= 0) {
-	    if ((Ra.top - P) <= R) {
-	      b.className = 'stop';
-	      b.style.top = - R +'px';
+	 $('.form-input').focus(function(){
+	    var label = $(this).next('.label-over');
+	    var width = $(label).width();
+	    var adjust = width;
+	    var value = $(this).val();
+
+	    if(value == ''){
+	        
+	        $(this).parent().addClass('focus')
 	    } else {
-	      b.className = 'sticky';
-	      b.style.top = P + 'px';
+
+	        
 	    }
-	  } else {
-	    b.className = '';
-	    b.style.top = '';
-	  }
-	  window.addEventListener('resize', function() {
-	    a.children[0].style.width = getComputedStyle(a, '').width
-	  }, false);
+	}).blur(function(){
+	    var label = $(this).next('.label-over');
+	    var value = $(this).val();
+	    if(value == ''){
+	       
+	        $(this).parent().removeClass('focus')
+
+	    } 
+	});
+	if($('.styler').length){
+		$('.styler').styler();
 	}
-})()
+	if($('.pricing-nav-menu').length){
+	
+		 (function(){
+		var a = document.querySelector('.pricing-nav-menu'), b = null, P = 150;
+		window.addEventListener('scroll', Ascroll, false);
+		document.body.addEventListener('scroll', Ascroll, false);
+		function Ascroll() {
+		  if (b == null) {
+		    var Sa = getComputedStyle(a, ''), s = '';
+		    for (var i = 0; i < Sa.length; i++) {
+		      if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+		        s += Sa[i] + ': ' +Sa.getPropertyValue(Sa[i]) + '; '
+		      }
+		    }
+		    b = document.createElement('div');
+		    b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+		    a.insertBefore(b, a.firstChild);
+		    var l = a.childNodes.length;
+		    for (var i = 1; i < l; i++) {
+		      b.appendChild(a.childNodes[1]);
+		    }
+		    a.style.height = b.getBoundingClientRect().height + 'px';
+		    a.style.padding = '0';
+		    a.style.border = '0';
+		  }
+		  var Ra = a.getBoundingClientRect(),
+		      R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('.pricing-content').getBoundingClientRect().bottom);  // селектор блока, при достижении нижнего края которого нужно открепить прилипающий элемент
+		  if ((Ra.top - P) <= 0) {
+		    if ((Ra.top - P) <= R) {
+		      b.className = 'stop';
+		      b.style.top = - R +'px';
+		    } else {
+		      b.className = 'sticky';
+		      b.style.top = P + 'px';
+		    }
+		  } else {
+		    b.className = '';
+		    b.style.top = '';
+		  }
+		  window.addEventListener('resize', function() {
+		    a.children[0].style.width = getComputedStyle(a, '').width
+		  }, false);
+		}
+	})()
+}
+
+
+
 	  
 
 });
