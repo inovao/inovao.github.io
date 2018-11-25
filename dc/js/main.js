4d8bb5999cdffad5060a4edba78ad6399f5f3c3b
inovao <31061655+inovao@users.noreply.github.com>
2018-11-25 20:41:10 +0200

    fix

--- dc/js/main.js
+++ dc/js/main.js
@@ -1,7 +1,15 @@
+/* -----------------------------------------------------------
+	01. PLAYER
+----------------------------------------------------------- */
+
 var Player = function() {
 	var _this = this,
 		audioPlayer = document.querySelectorAll('.audio-player'),
-		fixedPlayer = document.getElementById('fixed-player');	
+		fixedPlayer = document.getElementById('fixed-player'),
+		intervalTime = null,
+		countVisual = 0,
+		countTimeInterval = 0,
+		fixedPlayerFlag = true;
 
 	this.playOrPause = function(player) {
 		var parent = player.closest('.audio-player'),
@@ -15,15 +23,61 @@ var Player = function() {
 			btnPlay.classList.add('active');
 			btnFixedPlay.classList.add('active');
 			parent.classList.add('playing');
+
+			_this.initTimePlayer(player);			
 		} else {
 			track.pause();
 			btnPlay.classList.remove('active');
 			btnFixedPlay.classList.remove('active');
 			parent.classList.remove('playing');
+
+			clearInterval(intervalTime);
 		}
 
-		fixedPlayer.classList.add('active');
+		if (fixedPlayerFlag) {
+			fixedPlayer.classList.add('active');
+			_this.visualAudio(btnPlay);
+			fixedPlayerFlag = false;
+		}	
+
 		_this.fixedPlayerShow(track);
+	}
+
+	this.initTimePlayer = function(player) {
+		clearInterval(intervalTime);
+
+		var parent = player.closest('.audio-player'),
+			track = parent.querySelector('audio'),
+			durationTime = fixedPlayer.querySelector('.durtime-audio'),
+			currentTime = fixedPlayer.querySelector('.curtime-audio'),
+			visualAudio = fixedPlayer.querySelector('.visual-audio');
+
+		intervalTime = setInterval(function() {
+			var curMin = Math.floor(track.currentTime / 60),
+				curSec = Math.floor(track.currentTime - curMin * 60),
+				durMin = Math.floor(track.duration / 60),
+				durSec = Math.floor(track.duration - durMin * 60),
+				sec = curSec;
+
+			if (curMin < 10) curMin = '0' + curMin; 
+			if (curSec < 10) curSec = '0' + curSec; 
+			if (durMin < 10) durMin = '0' + durMin; 
+			if (durSec < 10) durSec = '0' + durSec; 
+
+			durationTime.innerHTML = durMin + ':' + durSec;
+			currentTime.innerHTML = curMin + ':' + curSec;
+
+			var span = visualAudio.querySelectorAll('span'),
+				setToggle = track.duration / span.length;
+
+			if (sec % Math.floor(setToggle + 0.6) == 0) {
+				span[countVisual].classList.add('active');
+				span[countVisual].setAttribute('data-current-time', track.currentTime);
+				countVisual++;
+			}			
+
+			countTimeInterval++
+		}, 1000);		
 	}
 
 	this.pausedAllTracks = function(audioPlayer) {
@@ -35,12 +89,69 @@ var Player = function() {
 		}
 	}
 
+	this.mutedPlayer = function(player) {
+		var parent = player.closest('.audio-player'),
+			track = parent.querySelector('audio'),
+			muted = fixedPlayer.querySelector('.muted-audio');
+
+		if (track.muted) {
+			track.muted = false;
+			muted.classList.remove('active');
+		} else {
+			track.muted = true;
+			muted.classList.add('active');
+		}
+	}
+
+	this.volumePlayer = function(player, volume) {
+		var parent = player.closest('.audio-player'),
+			track = parent.querySelector('audio');
+
+		track.volume = volume / 100;
+	} 
+
+	this.visualAudio = function(player) {
+		var parent = player.closest('.audio-player'),
+			track = parent.querySelector('audio'),
+			visualAudio = fixedPlayer.querySelector('.visual-audio'),
+			spanWidth = visualAudio.offsetWidth,
+			countSpan = 0,
+			posSpan = 0;
+
+		countVisual = 0;
+		countTimeInterval = 0;
+
+		var sp = visualAudio.querySelectorAll('span');
+
+		if (sp.length) {
+			for (var i = 0; i < sp.length; i++) {
+				sp[i].remove();
+			}
+		}
+
+		for (var i = 0; i < spanWidth; i += 5.9) {
+			if (countSpan >= spanWidth) break;
+			countSpan++;
+		}
+
+		for (var i = 0; i < countSpan; i++) {
+			var span = document.createElement('span');
+			span.style.left = posSpan + 'px';
+			span.style.top = getRandomInt(8, 5) + 'px';
+			visualAudio.appendChild(span);
+			posSpan += 5;
+		}
+	}
+
 	this.fixedPlayerShow = function(track) {
 		var parent = track.closest('.audio-player'),
 			btnPlay = fixedPlayer.querySelector('.play-audio'),
 			btnNext = fixedPlayer.querySelector('.next-audio'),
 			btnPrev = fixedPlayer.querySelector('.prev-audio'),
-			currentPlay = parent.querySelector('.play-audio');
+			currentPlay = parent.querySelector('.play-audio'),
+			btnMuted = fixedPlayer.querySelector('.muted-audio'),
+			btnVolume = fixedPlayer.querySelector('.volume-audio'),
+			visualAudio = fixedPlayer.querySelector('.visual-audio');
 
 		fixedPlayer.querySelector('.thumb-audio img').setAttribute('src', parent.querySelector('.thumb-audio img').getAttribute('src'));
 		fixedPlayer.querySelector('.title-date').innerHTML = parent.getAttribute('data-date');
@@ -54,9 +165,14 @@ var Player = function() {
 			if (parent.nextElementSibling != 'undefined') {
 				var next = parent.nextElementSibling,
 					btnPlay = next.querySelector('.play-audio');
+
 				_this.playOrPause(btnPlay);
+				_this.initTimePlayer(btnPlay);
+				_this.visualAudio(btnPlay);
 			} else {
 				_this.playOrPause(currentPlay);
+				_this.initTimePlayer(currentPlay);
+				_this.visualAudio(currentPlay);
 			}			
 		}
 
@@ -64,10 +180,23 @@ var Player = function() {
 			if (parent.previousElementSibling != 'undefined') {
 				var prev = parent.previousElementSibling,
 					btnPlay = prev.querySelector('.play-audio');
+
 				_this.playOrPause(btnPlay);
+				_this.initTimePlayer(btnPlay);
+				_this.visualAudio(btnPlay);
 			} else {
 				_this.playOrPause(currentPlay);
+				_this.initTimePlayer(currentPlay);
+				_this.visualAudio(currentPlay);
 			}			
+		}
+
+		btnMuted.onclick = function(e) {
+			_this.mutedPlayer(currentPlay);
+		}
+
+		btnVolume.onchange = function() {
+			_this.volumePlayer(currentPlay, this.value);
 		}
 	}
 
@@ -80,3 +209,34 @@ var Player = function() {
 }
 
 var player = new Player();
+
+function getRandomInt(max, min) {
+  return Math.floor(Math.random() * Math.floor(max) + min);
+}
+
+/* -----------------------------------------------------------
+	02. SLIDER HOME
+----------------------------------------------------------- */
+
+$('#slider-home ul').slick({
+	slidesToShow: 4,
+	slidesToScroll: 1,
+	prevArrow: '<button type="button" class="slick-prev">&#8249;</button>',
+	nextArrow: '<button type="button" class="slick-next">&#8250;</button>',
+	responsive: [
+	    {
+		    breakpoint: 1000,
+		    settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 1,
+		    }
+	    },
+	    {
+		    breakpoint: 600,
+		    settings: {
+		        slidesToShow: 1,
+		        slidesToScroll: 1,
+		    }
+	    }
+  ]
+});
