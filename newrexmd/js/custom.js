74406723e4f515caa44237eec5f9aafda62f087f
inovao <31061655+inovao@users.noreply.github.com>
2019-09-27 12:38:29 +0200

    453

--- newrexmd/js/custom.js
+++ newrexmd/js/custom.js
@@ -29,5 +29,29 @@ $(function(){
 		return false;
 	});
    
+    function parallax() {
+	   	$window = $(window);
+		$('[data-type="background"]').each(function(){
+		     var $bgobj = $(this);
+			$(window).scroll(function() {
+			    var yPos = -($window.scrollTop() / $bgobj.data('speed')); 
+			    var coords = '50% '+ yPos + 'px';
+			    $bgobj.css({ backgroundPosition: coords });
+			}); 
+		});    
+	}
+	parallax();
+
+	function slideLeft() {
+		$(window).on('load scroll', function() { 
+		    if ($(window).scrollTop() >= $('.standart-bg').offset().top  - window.innerHeight) { 
+		        $('.standart-bg').addClass('open');
+		    }
+		    else {
+		      $('.standart-bg').removeClass('open');
+		    }
+		});
+	}
+	slideLeft();
 
 });
