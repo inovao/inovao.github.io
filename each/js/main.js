967a46cbe0479ca4de9f5dc530216c735b59d7b7
inovao <31061655+inovao@users.noreply.github.com>
2019-11-22 13:24:12 +0200

    віаіва

---
+++ each/js/main.js
@@ -0,0 +1,44 @@
+/* --------------- MENU ----------------- */
+var togle = document.querySelector('.toggle-menu'),
+	nav = document.querySelector('.nav'),
+	close = document.querySelector('.nav .close');
+
+togle.addEventListener('click', function() {
+	nav.classList.add('fade');
+	return false;
+}, false);
+
+close.addEventListener('click', function(e) {
+	nav.classList.remove('fade');
+	return false;
+}, false);
+/* --------------- // MENU ----------------- */
+
+/* --------------- SLICK ----------------- */
+$('.home').slick({
+	infinite: true,
+	slidesToShow: 1,
+	slidesToScroll: 1,
+	autoplay: true,
+  	autoplaySpeed: 2000,
+  	arrows: false
+});
+
+$('.home-icons .visible-iphone').slick({
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 3,
+	autoplay: true,
+  	autoplaySpeed: 2000,
+  	arrows: false,
+  	responsive: [
+  		{
+		    breakpoint: 500,
+		    settings: {
+		        slidesToShow: 2,
+		        slidesToScroll: 2
+		    }
+	    },
+  	]
+});
+/* --------------- // SLICK ----------------- */
